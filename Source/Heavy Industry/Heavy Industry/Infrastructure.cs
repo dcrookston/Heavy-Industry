dda98150de992b7d3140849d53e3bbfc356e51e1
dcrookston <dcrookston@gmail.com>
2020-04-26 19:02:07 -0400

    Added C# project
    Created C# project

---
+++ Source/Heavy Industry/Heavy Industry/Infrastructure.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using Verse;
+
+namespace OBE_Heavy
+{
+    public class requiresInfrastructure : DefModExtension
+    {
+        public bool reqInf = false;
+        //public string infrastructureRequired;
+    }
+
+    abstract class Infrastructure : ThingDef
+    {
+        public bool InfrastructureProvided;
+        //public int SupportRange;
+    }
+   // public class Scaffolding : Infrastructure
+   // {
+   // }
+}
